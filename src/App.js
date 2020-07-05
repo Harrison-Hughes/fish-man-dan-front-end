4a3a93857e7a186846405b685239a17a34a00953
github username <harr.hughes@gmail.com>
2020-07-05 12:43:16 +0100

    setting app basis

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import API from "./adapters/API";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [user, setUser] = useState(false);
+  const [validatedUser, setValidatedUser] = useState(false);
+  const [error, setError] = useState(false);
+
+  const logOut = () => {
+    setUser(null);
+    API.clearToken();
+  };
+
+  useEffect(() => {
+    if (API.hasToken) {
+      API.validate()
+        .then(setUser)
+        .then(() => setValidatedUser(true))
+        .catch((errorPromise) => {
+          if (!errorPromise) errorPromise.then(setError);
+          else
+            setError({
+              message: "Server is currently offline. Please try later",
+            });
+        });
+    } else {
+      setValidatedUser(true);
+    }
+  }, []);
+
+  if (!validatedUser) return <LoadingScreen />;
+  else return <div className="App"></div>;
+};
 
 export default App;
