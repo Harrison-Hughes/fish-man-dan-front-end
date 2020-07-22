3b80a4a4ee2ea315bbcffbe47ea8dd53c207e946
github username <harr.hughes@gmail.com>
2020-07-22 16:23:25 +0100

    master push

--- src/authenticated-app/AuthenticatedApp.js
+++ src/authenticated-app/AuthenticatedApp.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-const AuthenticatedApp = (props) => {
-  return <div className="authenticated-app"></div>;
+const AuthenticatedApp = ({ user, setUser, setError, logOut }) => {
+  return (
+    <div className="authenticated-app">
+      <h1>hi</h1>
+    </div>
+  );
 };
 
 export default AuthenticatedApp;
